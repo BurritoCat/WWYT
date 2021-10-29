f78a454e22c5c60fc4d53f0d21bd8783a6f14a29
BurritoCat <63421600+BurritoCat@users.noreply.github.com>
2021-10-29 13:41:17 -0400

    Update SideCharacter.cs

--- ConversationCode/SideCharacter.cs
+++ ConversationCode/SideCharacter.cs
@@ -19,7 +19,15 @@ public class SideCharacter : MonoBehaviour
 
         else
         {
-            if(ConversationNode.nextConversations.Length == 1)
+            GameObject player = GameObject.FindWithTag("Player");
+            if (ConversationNode.affectsHealth)
+                player.GetComponent<Player>().changeHealth(ConversationNode.healthAmount);
+            if (ConversationNode.affectsMoney)
+                player.GetComponent<Player>().changeMoney(ConversationNode.moneyAmount);
+            if (ConversationNode.affectsTime)
+                player.GetComponent<Player>().changeTime();
+
+            if (ConversationNode.nextConversations.Length == 1)
             {
                 ConversationNode = ConversationNode.nextConversations[0];
                 Conversation = ConversationNode.stringsInConv;
@@ -28,18 +36,26 @@ public class SideCharacter : MonoBehaviour
 
             else
             {
-                characterFavor += characterPreferences[indexes[0]] + characterPreferences[indexes[1]];
                 int trueIndex = 0;
                 //Needs math eq to calculate number
                 trueIndex += indexes[0] * 3;
                 trueIndex += indexes[1] - 3;
+                characterFavor += characterPreferences[trueIndex];
 
                 ConversationNode = ConversationNode.nextConversations[trueIndex];
                 Conversation = ConversationNode.stringsInConv;
                 this.gameObject.GetComponent<Dialogue>().changeResponse(ConversationNode.needsResponse);
             }
+
         }
        
         return Conversation;
+    }
+
+    public void setUpConversation(DialogueTreeNode aNode)
+    {
+        ConversationNode = aNode;
+        Conversation = ConversationNode.stringsInConv;
+        this.gameObject.GetComponent<Dialogue>().changeResponse(ConversationNode.needsResponse);
     }
 }
